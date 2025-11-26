99fc6c5cccf5683fb2fe2aac582daa90b51bf479
SergioMaiorana <117360729+SergioMaiorana@users.noreply.github.com>
2025-11-26 02:16:38 -0300

    FINAL FIX: Corregido C-mayuscula a c-minuscula en todas las importaciones de App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
-
 import React from 'react';
-import Header from './Components/Header';
+import Header from './components/Header';
 import Hero from './components/Hero';
 import Services from './components/Services';
 import About from './components/About';
